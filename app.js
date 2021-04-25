a9919a18d09903ab39b9685f8137e8f013707437
Casey Colley <colleyc@oregonstate.edu>
2021-04-24 23:46:18 -0700

    added some spice

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 // Express
 var express = require("express");
 var app = express();
+const path = require("path");
 
 // Handlebars
 var handlebars = require("express-handlebars").create({
@@ -15,46 +16,49 @@ app.set("view engine", "handlebars");
 PORT = 3131;
 app.set("port", PORT);
 
-// Not quite sure what this does, but it seems to set the path for the static files (Ex: style.css)
-const path = require("path");
-const publicPath = path.resolve(__dirname, "public");
-app.use(express.static(publicPath));
+// Serves static files from "public" directory
+app.use(express.static("public"));
 
 // Database
 var db = require("./db-connector");
 
 /* ROUTES */
 
-app.get("/browse-catalog", function (req, res) {
-  //db.pool.query(query, function(err, results, fields) {});
-  const context = {};
+app.get("/", function(req, res) {
+	res.status(301).redirect("/browse-catalog");
+	next();
+});
 
-  res.render("browse-catalog", context);
+app.get("/browse-catalog", function (req, res) {
+	//db.pool.query(query, function(err, results, fields) {});
+	const context = {};
+
+	res.status(200).render("browse-catalog", context);
 });
 
 app.get("/manage-orders", function (req, res) {
-  //db.pool.query(query, function(err, results, fields) {});
-  const context = {};
+	//db.pool.query(query, function(err, results, fields) {});
+	const context = {};
 
-  res.render("manage-orders", context);
+	res.status(200).render("manage-orders", context);
 });
 
 app.get("/place-order", function (req, res) {
-  //db.pool.query(query, function(err, results, fields) {});
-  const context = {};
+	//db.pool.query(query, function(err, results, fields) {});
+	const context = {};
 
-  res.render("place-order", context);
+	res.status(200).render("place-order", context);
 });
 
 app.get("/add-book", function (req, res) {
-  //db.pool.query(query, function(err, results, fields) {});
-  const context = {};
+	//db.pool.query(query, function(err, results, fields) {});
+	const context = {};
 
-  res.render("add-book", context);
+	res.status(200).render("add-book", context);
 });
 
 /* LISTENER */
 
 app.listen(PORT, function () {
-  console.log("Listening on port " + PORT);
+	console.log("Listening on port " + PORT);
 });
